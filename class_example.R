ea51d7f38d87d3a46c17d11b6d658afd7d348fbc
Noah-R-Marcotte <70666277+Noah-R-Marcotte@users.noreply.github.com>
2022-09-12 14:28:41 -0400

    more updates

--- class_example.R
+++ class_example.R
@@ -19,3 +19,25 @@ for(i in 1:5){
 
 out
 
+
+mylist <- list(1, 2, 3, 'some', 'text', 2, 5)
+mylist
+
+sum(unlist(lapply(mylist, is.character)))
+
+sum(unlist(lapply(mylist, is.numeric)))
+
+
+summary <- function(x){
+  funs <- c(mean, median, sd, mad, IQR)
+  lapply(funs, function(f) f(x, na.rm = TRUE))
+}
+
+#function is the object here, being applied as object
+
+summary(c(1,2,3))
+summary(c(2,3,5,9,8,14,2))
+
+
+
+
